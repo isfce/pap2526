9fe3c578384520d4593d3dbf691338b795cc715b
VOZ <voisfce@gmail.com>
2026-01-06 21:53:08 +0100

    Vecteur tp2

--- PAP2526/src/tp01/MyVect.java
+++ PAP2526/src/tp01/MyVect.java
@@ -1,7 +1,5 @@
 package tp01;
 
-import java.util.Arrays;
-
 public class MyVect {
 
 	public static void affiche(int[] v) {
@@ -32,14 +30,100 @@ public class MyVect {
 		return s;
 	}
 
-	public static void main(String[] args) {
-		int[] vn = null;
-		int[] v0 = {};
-		int[] v1 = { 1, 1, 2, 3, 5, 8 };
-		System.out.println(v1);
-		System.out.println(Arrays.toString(v1));
-		affiche(vn);
-		affiche(v0);
-		affiche(v1);
+	/**
+	 * Retourne le nombre de fois que nbr existe ds le vecteur
+	 * 
+	 * @param v
+	 * @param nbr élément à rechercher
+	 * @return
+	 */
+	public static int nbElemV(int[] v, int nbr) {
+		assert v.length > 0 : "Le vecteur ne peut pas être vide";
+		int cpt = 0;
+		for (int elem : v)
+			if (elem == nbr)
+				cpt++;
+		return cpt;
+
 	}
+
+	/**
+	 * retourne le min et le max d'un vecteur
+	 * 
+	 * @param v
+	 * @return res[0]=min res[1]=max
+	 */
+	public static int[] minMaxElem(int[] v) {
+		assert v.length > 0 : "Le vecteur ne peut pas être vide";
+		int[] res = { v[0], v[0] };
+		for (int i = 1; i < v.length; i++)
+			if (v[i] < res[0])
+				res[0] = v[i];// autre min
+			else if (v[i] > res[1])
+				res[1] = v[i];// autre max
+		return res;
+	}
+
+	public static RMinMax minMaxElem2(int[] v) {
+		assert v.length > 0 : "Le vecteur ne peut pas être vide";
+		int min = v[0], max = v[0];
+		for (int i = 1; i < v.length; i++)
+			if (v[i] < min)
+				min = v[i];// autre min
+			else if (v[i] > max)
+				max = v[i];// autre max
+		return new RMinMax(min, max);
+	}
+
+	/**
+	 * clone 2 vecteurs
+	 * 
+	 * @param v
+	 * @return
+	 */
+	public static int[] clone(int[] v) {
+		int[] res = new int[v.length];
+		for (int i = 0; i < v.length; i++)
+			res[i] = v[i];
+		return res;
+	}
+
+	public static boolean estPalindrome(char[] txt) {
+		int i = 0;
+		int j = txt.length - 1;
+		boolean palindrome = true;
+		while (palindrome && i < j) {
+			palindrome = txt[i] == txt[j];
+			i++;
+			j--;
+		}
+		return palindrome;
+	}
+
+	/**
+	 * Inverse le contenu du vecteur
+	 * 
+	 * @param v
+	 */
+	public static void inverseV(int[] v) {
+		int i = 0;
+		int j = v.length - 1;
+		int tmp;
+		while (i < j) {
+			// swap v[i] avec v[j]
+			tmp = v[i];
+			v[i] = v[j];
+			v[j] = tmp;
+			// avance
+			i++;
+			j--;
+		}
+	}
+
+	public static void main(String[] args) {
+	}
+
 }
+
+record RMinMax(int min, int max) {
+};
--- PAP2526/test/tp01/TestMyVect.java
+++ PAP2526/test/tp01/TestMyVect.java
@@ -1,7 +1,10 @@
 package tp01;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -13,5 +16,60 @@ public class TestMyVect {
 		assertEquals(0, MyVect.sommeV(v0));
 		assertEquals(4, MyVect.sommeV(v1));
 		assertThrows(AssertionError.class, () -> MyVect.sommeV(null));
+	}
+
+	@Test
+	public void testNbElem() {
+		int[] v0 = {};
+		int[] v1 = { 1, 2, 3, 0, 1, 2, 2, 8 };
+		assertEquals(1, MyVect.nbElemV(v1, 0));
+		assertEquals(3, MyVect.nbElemV(v1, 2));
+		assertEquals(2, MyVect.nbElemV(v1, 1));
+		assertEquals(1, MyVect.nbElemV(v1, 8));
+		assertEquals(0, MyVect.nbElemV(v1, 88));
+		assertThrows(AssertionError.class, () -> MyVect.nbElemV(v0, 0));
+	}
+
+	@Test
+	public void testMinMax() {
+		int[] v0 = {};
+		int[] v1 = { 1, 50, 3, 1 };
+		assertArrayEquals(new int[] { 1, 50 }, MyVect.minMaxElem(v1));
+		assertThrows(AssertionError.class, () -> MyVect.minMaxElem(v0));
+	}
+
+	@Test
+	public void testMinMax2() {
+		int[] v0 = {};
+		int[] v1 = { 1, 50, 3, 1 };
+		assertEquals(1, MyVect.minMaxElem2(v1).min());
+		assertEquals(50, MyVect.minMaxElem2(v1).max());
+		assertEquals(new RMinMax(1, 50), MyVect.minMaxElem2(v1));
+		assertThrows(AssertionError.class, () -> MyVect.minMaxElem2(v0));
+	}
+
+	@Test
+	public void testClone() {
+		int[] v0 = {};
+		int[] v1 = { 1, 5, -2 };
+		assertArrayEquals(new int[] { 1, 5, -2 }, MyVect.clone(v1));
+		assertArrayEquals(new int[] {}, MyVect.clone(v0));
+	}
+
+	@Test
+	public void testPalindrome() {
+		assertTrue(MyVect.estPalindrome("RADAR".toCharArray()));
+		assertTrue(MyVect.estPalindrome("ABBA".toCharArray()));
+		assertTrue(MyVect.estPalindrome("A".toCharArray()));
+		assertFalse(MyVect.estPalindrome("ABTFBA".toCharArray()));
+		assertFalse(MyVect.estPalindrome("zBBA".toCharArray()));
+	}
+
+	@Test
+	public void testInverse() {
+		int[] v1 = { 1, 5, -2 };
+		MyVect.inverseV(v1);
+		assertArrayEquals(new int[] { -2, 5, 1 }, v1);
+
 	}
 }
